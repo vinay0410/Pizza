2b9eec9e4c2c8d95d97d65f280118e26b83f0d9a
Vinay Sharma <vinay04sharma@icloud.com>
2017-09-16 00:08:21 +0530

    just added forms

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <!DOCTYPE html>
-<?php
-  echo '<h1>Hello World!</h1>'
- ?>
+<html>
+<head><title>blah blah Pizza Chain</title></head>
+<body>
+<div class="login-signup">
+  <form action="login.php" method="post">
+    <input name="username" type="email" placeholder="Enter your Email ID" required>
+    <input name="pass" type="password" placeholder="Enter your password" required>
+    <input type="submit" value="Login">
+  </form>
+    <button onclick="togglesignup();">Sign Up</button>
+</div>
+<div class="signup-modal" id="signup" style="display: none">
+  <form action="signup.php" method="post">
+  <input name="name" type="text" placeholder="Enter your name" required>
+  <input name="email" type="email" placeholder="Enter your Email ID" required>
+  <input name="pass" type="password" placeholder="Enter your password" required>
+
+  <input type="submit" value="Sign Up">
+</form>
+</div>
+</body>
+
+<script src="main.js"></script>
+</html>
---
+++ main.js
@@ -0,0 +1,6 @@
+function togglesignup() {
+  var x = document.getElementById('signup')
+  console.log(x);
+  x.style.display = "block";
+  x.className += "visible";
+}
