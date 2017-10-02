f84defa263ccc8fb2ab13282344cc4cf56a5dddf
Vinay Sharma <vinay04sharma@icloud.com>
2017-10-02 19:06:17 +0530

    using sag changed

--- mydb.php
+++ mydb.php
@@ -2,28 +2,30 @@
 // Licensed under the Apache License. See footer for details.
 
 
- //We need to pull in the Sag PHP library. SAG is an open API used to connect to the Cloudant database. 
+ //We need to pull in the Sag PHP library. SAG is an open API used to connect to the Cloudant database.
  //We only need to do this once!
+ echo "Started"
  require('vendor/autoload.php');
+ echo "I didn't reach here"
 
-//Get Connection Variables from VCAPS_SERVICES. We first need to pull in our Cloudant database 
-//connection variables from the VCAPS_SERVICES environment variable. This environment variable 
+//Get Connection Variables from VCAPS_SERVICES. We first need to pull in our Cloudant database
+//connection variables from the VCAPS_SERVICES environment variable. This environment variable
 //will be put in your project by Bluemix once you add the Cloudant database to your Bluemix
-//application. 
+//application.
 
-// vcap_services Extraction 
+// vcap_services Extraction
 $services_json = json_decode(getenv('VCAP_SERVICES'),true);
 $VcapSvs = $services_json["cloudantNoSQLDB"][0]["credentials"];
 
-//Debug: If you want to see all the variables returned you can use this line of code. 
-//var_dump($services_json); 
-
-// Extract the VCAP_SERVICES variables for Cloudant connection.  
+//Debug: If you want to see all the variables returned you can use this line of code.
+var_dump($services_json);
+echo "above is debugging"
+// Extract the VCAP_SERVICES variables for Cloudant connection.
  $myUsername = $VcapSvs["username"];
  $myPassword = $VcapSvs["password"];
- 
+
  try {
-  // Let's login to the database. 
+  // Let's login to the database.
   $sag = new Sag($myUsername . ".cloudant.com");
   $sag->login($myUsername, $myPassword);
 
@@ -38,19 +40,19 @@ $VcapSvs = $services_json["cloudantNoSQLDB"][0]["credentials"];
   } else {
 
 	  // We are now going to read a document from our cloudant database. We are going
-	  // to retrieve the value associated with myKey from the body of the document. 
+	  // to retrieve the value associated with myKey from the body of the document.
   	  //The SAG PHP library takes care of all the gory details and only retrieves the value.
 	  $resp = $sag->get('myId')->body->myKey;
 
-	  echo $resp;  
+	  echo $resp;
     }
-         
-  // Assuming everything above was executed without error, we now are connected to the 
+
+  // Assuming everything above was executed without error, we now are connected to the
   // database and have retrieved the value.
-  
+
   //NOTE: Since we have a connection to the database, we can query the database for other
-  //      documents and retrieve other variables at a later time. We do not need to connect 
-  //      to the database again. 
+  //      documents and retrieve other variables at a later time. We do not need to connect
+  //      to the database again.
 
 
 }
