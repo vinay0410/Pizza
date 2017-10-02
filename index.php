23ce07aa835f5a85f17695570029c769437d4730
Vinay Sharma <vinay04sharma@icloud.com>
2017-10-02 18:09:21 +0530

    home page changed

--- index.php
+++ index.php
@@ -1,240 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
+<?php
+// use Composer autoloader
+require 'vendor/autoload.php';
+require 'config.php';
 
-	<title>Pizza - free responsive website template</title>
-	<meta name="keywords" content="">
-	<meta name="description" content="">
-    <meta name="author" content="templatemo">
-    <!-- 
-    Pizza Template
-    http://www.templatemo.com/preview/templatemo_459_pizza
-    -->
-    
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+// configure Slim application instance
+// initialize application
+$app = new \Slim\Slim(array(
+  'debug' => true,
+  'templates.path' => './views'
+));
 
-	<!-- bootstrap -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<!-- font-awesome -->
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<!-- custom -->
-	<link rel="stylesheet" href="css/templatemo-style.css">
-	<!-- google font -->
-	<link href='//fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
-	<link href='//fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'>
+$app->config = $config;
 
-</head>
-<body id="home" data-spy="scroll" data-target=".navbar-collapse">
+// extract database name from URI
+// initialize PHP Mongo client
+$dbn = substr(parse_url($app->config['db_uri'], PHP_URL_PATH), 1);
+$mongo = new MongoClient($app->config['db_uri'], array("connectTimeoutMS" => 30000));
+$db = $mongo->selectDb($dbn);
 
-	<!-- start navigation -->
-	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<div class="container">
-			<div class="navbar-header">
-				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon icon-bar"></span>
-					<span class="icon icon-bar"></span>
-					<span class="icon icon-bar"></span>
-				</button>
-				<a href="#home" class="navbar-brand smoothScroll"><strong>PIZZA</strong></a>
-			</div>
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#home" class="smoothScroll">HOME</a></li>
-					<li><a href="#about" class="smoothScroll">ABOUT</a></li>
-					<li><a href="#gallery" class="smoothScroll">GALLERY</a></li>
-					<li><a href="#contact" class="smoothScroll">CONTACT</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<!-- end navigation -->
+// index page handlers
+$app->get('/', function () use ($app) {
+  $app->redirect($app->urlFor('index'));
+});
 
-	<!-- start flexslider -->
-	<div class="flexslider">
-		<ul class="slides">
-			<li>
-				<img src="images/slider-img1.jpg" alt="Pizza Image 1">
-				<div class="flex-caption">
-					<h2 class="slider-title">We make Pizza</h2>
-					<h3 class="slider-subtitle">Fresh, clean, and delicious.</h3>
-					<p class="slider-description">Praesent tincidunt neque semper elementum gravida. Donec id euismod magna. Ut erat ligula, malesuada eu quam a, fringilla auctor augue.</p>
-				</div>
-			</li>
-			<li>
-				<img src="images/slider-img2.jpg" alt="Pizza Image 2">
-				<div class="flex-caption">
-					<h2 class="slider-title">Freshly Baked Pizza</h2>
-					<h3 class="slider-subtitle">Premium Quality, Finest Ingredients</h3>
-					<p class="slider-description">Donec id euismod magna. Ut erat ligula, malesuada eu quam a, fringilla auctor augue. Praesent tincidunt neque semper elementum gravida.</p>
-				</div>
-			</li>
-		</ul>
-	</div>
-	<!-- end flexslider -->
+// handler to list available notes in database
+// if query string included
+// filter results to match query string
+$app->get('/index', function () use ($app, $db) {
+  $collection = $db->notes;  
+  $q = trim(strip_tags($app->request->get('q')));
+  $where = array();
+  if (!empty($q)) {
+    $where = array(
+      '$or' => 
+        array(
+          array(
+            'title' => array('$regex' => new MongoRegex("/$q/i"))), 
+          array(
+            'body' => array('$regex' => new MongoRegex("/$q/i")))
+        )
+    );  
+  }
+  $notes = $collection->find($where)->sort(array('updated' => -1));
+  $app->render('index.tpl.php', array('notes' => $notes));
+})->name('index');
 
-	<!-- start about -->
-	<section id="about" class="templatemo-section templatemo-top-130">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					<h1 class="text-uppercase">Your Pizza Shop</h1>
-				</div>
-				<div class="col-md-6 col-sm-6">					
-					<h3 class="text-uppercase padding-bottom-10">Another Baker</h3>
-					<p>Pizza responsive web template is provided by <a rel="nofollow" href="http://www.templatemo.com" target="_parent">templatemo</a> website. Feel free to download, adapt, and use this template for your websites. Credit goes to <a rel="nofollow" href="http://pixabay.com" target="_parent">Pixabay</a> for images used in this template.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
-					<p>Proin enim sem, ultricies sit amet convallis nec, sodales quis augue. Duis consequat felis ac justo luctus, a cursus tellus pharetra. In ullamcorper gravida enim id pulvinar.</p>
-				</div>
-				<div class="col-md-6 col-sm-6">
-					<img src="images/about-img1.jpg" class="img-responsive img-bordered img-about" alt="about img">
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- end about -->
+// handler to display add/edit form
+$app->get('/save(/:id)', function ($id = null) use ($app, $db) {
+  $collection = $db->notes;
+  $note = $collection->findOne(array('_id' => new MongoId($id)));
+  $app->render('save.tpl.php', array('note' => $note));
+});
 
-	<!-- start gallery -->
-	<section id="gallery" class="templatemo-section templatemo-light-gray-bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h2 class="text-center text-uppercase">Gallery</h2>
-					<hr>
-				</div>
-				<div class="col-md-4 col-sm-4">
-					<div class="gallery-wrapper">
-						<img src="images/gallery-img1.jpg" class="img-responsive gallery-img" alt="Pizza 1">
-						<div class="gallery-des">
-							<h3>Curabitur </h3>
-							<h5>Cras in ante mattis, elementum nunc sed.</h5>
-						</div>
-					</div>
-				</div>	
-				<div class="col-md-4 col-sm-4">
-					<div class="gallery-wrapper">
-						<img src="images/gallery-img2.jpg" class="img-responsive gallery-img" alt="Pizza 2">
-						<div class="gallery-des">
-							<h3>Lorem ipsum</h3>
-							<h5>In ullamcorper gravida enim id pulvinar</h5>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-4">
-					<div class="gallery-wrapper">
-						<img src="images/gallery-img3.jpg" class="img-responsive gallery-img" alt="Pizza 3">
-						<div class="gallery-des">
-							<h3>Pellentesque</h3>
-							<h5>Maecenas efficitur nisi id sapien</h5>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 col-sm-6">
-					<div class="gallery-wrapper">
-						<img src="images/gallery-img4.jpg" class="img-responsive gallery-img" alt="Pizza 4">
-						<div class="gallery-des">
-							<h3>Suspendisse</h3>
-							<h5>Mauris sit amet augue sit amet risus</h5>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 col-sm-6">
-					<div class="gallery-wrapper">
-						<img src="images/gallery-img5.jpg" class="img-responsive gallery-img" alt="Pizza 5">
-						<div class="gallery-des">
-							<h3>Elementum</h3>
-							<h5>Maecenas efficitur nisi id sapien</h5>
-						</div>
-					</div>
-				</div>				
-			</div>
-		</div>
-	</section>
-	<!-- end gallery -->
+// handler to process form input 
+// save note content to database
+$app->post('/save', function () use ($app, $db) {
+  $collection = $db->notes;  
+  $id = trim(strip_tags($app->request->post('id')));
+  $note = new stdClass;
+  $note->title = trim(strip_tags($app->request->post('title')));
+  $note->body = trim(strip_tags($app->request->post('body')));
+  $note->color = trim(strip_tags($app->request->post('color')));
+  $note->updated = time();
+  if (!empty($id)) {
+    $note->_id = new MongoId($id);
+  }
+  $collection->save($note);
+  $app->redirect($app->urlFor('index'));
+});
 
-	<!-- start contact -->
-	<section id="contact" class="templatemo-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h2 class="text-uppercase text-center">Contact Us</h2>
-					<hr>
-				</div>
-				<div class="col-md-2"></div>
-				<div class="col-md-8">
-					<form action="#" method="post" role="form">
-						<div class="col-md-6 col-sm-6">
-							<input name="name" type="text" class="form-control" id="name" maxlength="60" placeholder="Name">
-					    	<input name="email" type="email" class="form-control" id="email" placeholder="Email">
-							<input name="message" type="text" class="form-control" id="message" placeholder="Subject">
-						</div>
-						<div class="col-md-6 col-sm-6">
-							<textarea class="form-control" rows="5" placeholder="Message"></textarea>
-						</div>
-						<div class="col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6">
-							<input name="submit" type="submit" class="form-control" id="submit" value="Send">
-						</div>
-					</form>
-				</div>
-				<div class="col-md-2"></div>
-				<div class="col-md-4 col-sm-4">
-					<h3 class="padding-bottom-10 text-uppercase">Visit our shop</h3>
-					<p><i class="fa fa-map-marker contact-fa"></i> 63 Another Walking Street, BKK 18080</p>
-					<p>
-						<i class="fa fa-phone contact-fa"></i> 
-						<a href="tel:010-020-0340" class="contact-link">010-020-0340</a>, 
-						<a href="tel:080-090-0660" class="contact-link">080-090-0660</a>
-					</p>			
-					<p><i class="fa fa-envelope-o contact-fa"></i> 
-                    	<a href="mailto:hello@company.com" class="contact-link">hello@company.com</a></p>
-				</div>
-				<div class="col-md-4 col-sm-4">
-					<h3 class="text-uppercase">Opening hours</h3>
-					<p><i class="fa fa-clock-o contact-fa"></i> 7:00 AM - 11:00 PM</p>
-					<p><i class="fa fa-bell-o contact-fa"></i> Monday to Friday and Sunday</p>
-                    <p><i class="fa fa-download contact-fa"></i> 
-                    	<a class="contact-link" rel="nofollow" 
-                        	href="http://fontawesome.io/icons/" target="_blank">Change Icons</a></p>
-			  	</div>
-				<div class="col-md-4 col-sm-4">
-					<div class="google_map">
-						<div id="map-canvas" class="map"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- end contact -->
+// handler to delete specified note 
+$app->get('/delete/:id', function ($id) use ($app, $db) {
+  $collection = $db->notes;
+  $collection->remove(array('_id' => new MongoId($id)));
+  $app->redirect($app->urlFor('index'));
+});
 
-	<!-- start footer -->
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<p>Copyright &copy; 2084 Company Name</p>
-					<hr>
-					<ul class="social-icon">
-						<li><a href="#" class="fa fa-facebook"></a></li>
-						<li><a href="#" class="fa fa-twitter"></a></li>
-						<li><a href="#" class="fa fa-instagram"></a></li>
-						<li><a href="#" class="fa fa-pinterest"></a></li>
-						<li><a href="#" class="fa fa-google"></a></li>
-						<li><a href="#" class="fa fa-github"></a></li>
-						<li><a href="#" class="fa fa-apple"></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</footer>
-	<!-- end footer -->
+// handler to display specified note
+$app->get('/view/:id', function ($id) use ($app, $db) {
+  $collection = $db->notes;
+  $note = $collection->findOne(array('_id' => new MongoId($id)));
+  $app->render('view.tpl.php', array('note' => $note));
+});
 
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/plugins.js"></script>
-	<script src="js/smoothscroll.js"></script>
-	<script src="js/custom.js"></script>
+// handler to display legal terms
+$app->get('/legal', function () use ($app) {
+  $app->render('legal.tpl.php');
+});
 
-</body>
-</html>
+
+// hook to add request URI path as template variable
+$app->hook('slim.before.dispatch', function() use ($app) {
+  $app->view()->appendData(array(
+    'baseUri' => $app->request()->getRootUri()
+  ));
+}); 
+
+$app->run();
---
+++ index_old.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>Pizza - free responsive website template</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+    <meta name="author" content="templatemo">
+    <!-- 
+    Pizza Template
+    http://www.templatemo.com/preview/templatemo_459_pizza
+    -->
+    
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- font-awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- custom -->
+	<link rel="stylesheet" href="css/templatemo-style.css">
+	<!-- google font -->
+	<link href='//fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'>
+
+</head>
+<body id="home" data-spy="scroll" data-target=".navbar-collapse">
+
+	<!-- start navigation -->
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+				</button>
+				<a href="#home" class="navbar-brand smoothScroll"><strong>PIZZA</strong></a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#home" class="smoothScroll">HOME</a></li>
+					<li><a href="#about" class="smoothScroll">ABOUT</a></li>
+					<li><a href="#gallery" class="smoothScroll">GALLERY</a></li>
+					<li><a href="#contact" class="smoothScroll">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- end navigation -->
+
+	<!-- start flexslider -->
+	<div class="flexslider">
+		<ul class="slides">
+			<li>
+				<img src="images/slider-img1.jpg" alt="Pizza Image 1">
+				<div class="flex-caption">
+					<h2 class="slider-title">We make Pizza</h2>
+					<h3 class="slider-subtitle">Fresh, clean, and delicious.</h3>
+					<p class="slider-description">Praesent tincidunt neque semper elementum gravida. Donec id euismod magna. Ut erat ligula, malesuada eu quam a, fringilla auctor augue.</p>
+				</div>
+			</li>
+			<li>
+				<img src="images/slider-img2.jpg" alt="Pizza Image 2">
+				<div class="flex-caption">
+					<h2 class="slider-title">Freshly Baked Pizza</h2>
+					<h3 class="slider-subtitle">Premium Quality, Finest Ingredients</h3>
+					<p class="slider-description">Donec id euismod magna. Ut erat ligula, malesuada eu quam a, fringilla auctor augue. Praesent tincidunt neque semper elementum gravida.</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<!-- end flexslider -->
+
+	<!-- start about -->
+	<section id="about" class="templatemo-section templatemo-top-130">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<h1 class="text-uppercase">Your Pizza Shop</h1>
+				</div>
+				<div class="col-md-6 col-sm-6">					
+					<h3 class="text-uppercase padding-bottom-10">Another Baker</h3>
+					<p>Pizza responsive web template is provided by <a rel="nofollow" href="http://www.templatemo.com" target="_parent">templatemo</a> website. Feel free to download, adapt, and use this template for your websites. Credit goes to <a rel="nofollow" href="http://pixabay.com" target="_parent">Pixabay</a> for images used in this template.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
+					<p>Proin enim sem, ultricies sit amet convallis nec, sodales quis augue. Duis consequat felis ac justo luctus, a cursus tellus pharetra. In ullamcorper gravida enim id pulvinar.</p>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<img src="images/about-img1.jpg" class="img-responsive img-bordered img-about" alt="about img">
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end about -->
+
+	<!-- start gallery -->
+	<section id="gallery" class="templatemo-section templatemo-light-gray-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="text-center text-uppercase">Gallery</h2>
+					<hr>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="gallery-wrapper">
+						<img src="images/gallery-img1.jpg" class="img-responsive gallery-img" alt="Pizza 1">
+						<div class="gallery-des">
+							<h3>Curabitur </h3>
+							<h5>Cras in ante mattis, elementum nunc sed.</h5>
+						</div>
+					</div>
+				</div>	
+				<div class="col-md-4 col-sm-4">
+					<div class="gallery-wrapper">
+						<img src="images/gallery-img2.jpg" class="img-responsive gallery-img" alt="Pizza 2">
+						<div class="gallery-des">
+							<h3>Lorem ipsum</h3>
+							<h5>In ullamcorper gravida enim id pulvinar</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="gallery-wrapper">
+						<img src="images/gallery-img3.jpg" class="img-responsive gallery-img" alt="Pizza 3">
+						<div class="gallery-des">
+							<h3>Pellentesque</h3>
+							<h5>Maecenas efficitur nisi id sapien</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<div class="gallery-wrapper">
+						<img src="images/gallery-img4.jpg" class="img-responsive gallery-img" alt="Pizza 4">
+						<div class="gallery-des">
+							<h3>Suspendisse</h3>
+							<h5>Mauris sit amet augue sit amet risus</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<div class="gallery-wrapper">
+						<img src="images/gallery-img5.jpg" class="img-responsive gallery-img" alt="Pizza 5">
+						<div class="gallery-des">
+							<h3>Elementum</h3>
+							<h5>Maecenas efficitur nisi id sapien</h5>
+						</div>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</section>
+	<!-- end gallery -->
+
+	<!-- start contact -->
+	<section id="contact" class="templatemo-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="text-uppercase text-center">Contact Us</h2>
+					<hr>
+				</div>
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
+					<form action="#" method="post" role="form">
+						<div class="col-md-6 col-sm-6">
+							<input name="name" type="text" class="form-control" id="name" maxlength="60" placeholder="Name">
+					    	<input name="email" type="email" class="form-control" id="email" placeholder="Email">
+							<input name="message" type="text" class="form-control" id="message" placeholder="Subject">
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<textarea class="form-control" rows="5" placeholder="Message"></textarea>
+						</div>
+						<div class="col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6">
+							<input name="submit" type="submit" class="form-control" id="submit" value="Send">
+						</div>
+					</form>
+				</div>
+				<div class="col-md-2"></div>
+				<div class="col-md-4 col-sm-4">
+					<h3 class="padding-bottom-10 text-uppercase">Visit our shop</h3>
+					<p><i class="fa fa-map-marker contact-fa"></i> 63 Another Walking Street, BKK 18080</p>
+					<p>
+						<i class="fa fa-phone contact-fa"></i> 
+						<a href="tel:010-020-0340" class="contact-link">010-020-0340</a>, 
+						<a href="tel:080-090-0660" class="contact-link">080-090-0660</a>
+					</p>			
+					<p><i class="fa fa-envelope-o contact-fa"></i> 
+                    	<a href="mailto:hello@company.com" class="contact-link">hello@company.com</a></p>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<h3 class="text-uppercase">Opening hours</h3>
+					<p><i class="fa fa-clock-o contact-fa"></i> 7:00 AM - 11:00 PM</p>
+					<p><i class="fa fa-bell-o contact-fa"></i> Monday to Friday and Sunday</p>
+                    <p><i class="fa fa-download contact-fa"></i> 
+                    	<a class="contact-link" rel="nofollow" 
+                        	href="http://fontawesome.io/icons/" target="_blank">Change Icons</a></p>
+			  	</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="google_map">
+						<div id="map-canvas" class="map"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end contact -->
+
+	<!-- start footer -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>Copyright &copy; 2084 Company Name</p>
+					<hr>
+					<ul class="social-icon">
+						<li><a href="#" class="fa fa-facebook"></a></li>
+						<li><a href="#" class="fa fa-twitter"></a></li>
+						<li><a href="#" class="fa fa-instagram"></a></li>
+						<li><a href="#" class="fa fa-pinterest"></a></li>
+						<li><a href="#" class="fa fa-google"></a></li>
+						<li><a href="#" class="fa fa-github"></a></li>
+						<li><a href="#" class="fa fa-apple"></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- end footer -->
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/custom.js"></script>
+
+</body>
+</html>
