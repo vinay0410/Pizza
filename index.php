2f5e5d6f14a4f94c06c3163be175e8cc6bfc8604
Vinay Sharma <vinay04sharma@icloud.com>
2017-10-03 11:59:00 +0530

    added register Login

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 	<meta name="keywords" content="">
 	<meta name="description" content="">
     <meta name="author" content="templatemo">
-    <!-- 
+    <!--
     Pizza Template
     http://www.templatemo.com/preview/templatemo_459_pizza
     -->
-    
+
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -26,8 +26,12 @@
 	<link href='//fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
 	<link href='//fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'>
 
+
 </head>
 <body id="home" data-spy="scroll" data-target=".navbar-collapse">
+
+
+
 
 	<!-- start navigation -->
 	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -46,7 +50,9 @@
 					<li><a href="#about" class="smoothScroll">ABOUT</a></li>
 					<li><a href="#gallery" class="smoothScroll">GALLERY</a></li>
 					<li><a href="#contact" class="smoothScroll">CONTACT</a></li>
-				</ul>
+					<li><a><button class="btn btn-warning pb-modalreglog-submit" data-toggle="modal" data-target="#myModal">Login</button><button class="btn btn-warning pb-modalreglog-submit" data-toggle="modal" data-target="#myModal2">Register</button></a></li>
+          <!--<li><a><button class="btn btn-warning pb-modalreglog-submit" data-toggle="modal" data-target="#myModal2">Register</button></a></li>-->
+
 			</div>
 		</div>
 	</div>
@@ -82,11 +88,13 @@
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<h1 class="text-uppercase">Your Pizza Shop</h1>
 				</div>
-				<div class="col-md-6 col-sm-6">					
+				<div class="col-md-6 col-sm-6">
 					<h3 class="text-uppercase padding-bottom-10">Another Baker</h3>
 					<p>Pizza responsive web template is provided by <a rel="nofollow" href="http://www.templatemo.com" target="_parent">templatemo</a> website. Feel free to download, adapt, and use this template for your websites. Credit goes to <a rel="nofollow" href="http://pixabay.com" target="_parent">Pixabay</a> for images used in this template.</p>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
 					<p>Proin enim sem, ultricies sit amet convallis nec, sodales quis augue. Duis consequat felis ac justo luctus, a cursus tellus pharetra. In ullamcorper gravida enim id pulvinar.</p>
+					<button class="btn btn-warning pb-modalreglog-submit" data-toggle="modal" data-target="#myModal">Login</button>
+          <button class="btn btn-warning pb-modalreglog-submit" data-toggle="modal" data-target="#myModal2">Register</button>
 				</div>
 				<div class="col-md-6 col-sm-6">
 					<img src="images/about-img1.jpg" class="img-responsive img-bordered img-about" alt="about img">
@@ -112,7 +120,7 @@
 							<h5>Cras in ante mattis, elementum nunc sed.</h5>
 						</div>
 					</div>
-				</div>	
+				</div>
 				<div class="col-md-4 col-sm-4">
 					<div class="gallery-wrapper">
 						<img src="images/gallery-img2.jpg" class="img-responsive gallery-img" alt="Pizza 2">
@@ -148,7 +156,7 @@
 							<h5>Maecenas efficitur nisi id sapien</h5>
 						</div>
 					</div>
-				</div>				
+				</div>
 			</div>
 		</div>
 	</section>
@@ -183,19 +191,19 @@
 					<h3 class="padding-bottom-10 text-uppercase">Visit our shop</h3>
 					<p><i class="fa fa-map-marker contact-fa"></i> 63 Another Walking Street, BKK 18080</p>
 					<p>
-						<i class="fa fa-phone contact-fa"></i> 
-						<a href="tel:010-020-0340" class="contact-link">010-020-0340</a>, 
+						<i class="fa fa-phone contact-fa"></i>
+						<a href="tel:010-020-0340" class="contact-link">010-020-0340</a>,
 						<a href="tel:080-090-0660" class="contact-link">080-090-0660</a>
-					</p>			
-					<p><i class="fa fa-envelope-o contact-fa"></i> 
+					</p>
+					<p><i class="fa fa-envelope-o contact-fa"></i>
                     	<a href="mailto:hello@company.com" class="contact-link">hello@company.com</a></p>
 				</div>
 				<div class="col-md-4 col-sm-4">
 					<h3 class="text-uppercase">Opening hours</h3>
 					<p><i class="fa fa-clock-o contact-fa"></i> 7:00 AM - 11:00 PM</p>
 					<p><i class="fa fa-bell-o contact-fa"></i> Monday to Friday and Sunday</p>
-                    <p><i class="fa fa-download contact-fa"></i> 
-                    	<a class="contact-link" rel="nofollow" 
+                    <p><i class="fa fa-download contact-fa"></i>
+                    	<a class="contact-link" rel="nofollow"
                         	href="http://fontawesome.io/icons/" target="_blank">Change Icons</a></p>
 			  	</div>
 				<div class="col-md-4 col-sm-4">
@@ -207,7 +215,127 @@
 		</div>
 	</section>
 	<!-- end contact -->
+	<!-- modals start -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+			<div class="modal-content">
+						<div class="modal-header">
 
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																				<span aria-hidden="true">&times;</span>
+																		</button>
+
+																	 <h4 class="modal-title" id="myModalLabel">Login form</h4>
+															</div><!--ending modal header-->
+
+
+						<div class="modal-body">
+																		<form >
+																				<div class="form-group">
+																						<label for="username">Username</label>
+									<div class="input-group pb-modalreglog-input-group">
+										<input id="username" class="form-control"  type="text" name="username" placeholder="Username"/>
+																									<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+									</div>
+										</div>
+
+
+								<div class="form-group">
+																							<label for="password">Password</label>
+																							<div class="input-group pb-modalreglog-input-group">
+																									<input type="password" class="form-control" id="pws" placeholder="Password">
+																									<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+																						</div>
+																				</div>
+
+
+					<a  href="forget.php" target="_blank">Forgot Password? </a>
+					</br></br>
+					<div class="modal-footer">
+																	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+																	<button type="submit" class="btn btn-warning">Log in</button>
+														</div> <!-- ending modal-footer -->
+
+
+																		</form>`
+
+														</div><!-- modal-body-->
+
+
+
+<!--login form ends-->
+											</div><!--modal-content-->
+
+
+							</div><!-- modal-dialog -->
+					</div>
+
+					<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-sm" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <h4 class="modal-title" id="myModalLabel">Registration form</h4>
+                        </div><!--modal header-->
+                        <div class="modal-body">
+
+				<!--form for registration -->
+                            <form class="pb-modalreglog-form-reg"  action="admin.php" onsubmit="return Validation();">
+				<div class="form-group">
+                                   	<label for="username">Username</label>
+ 					<div class="input-group pb-modalreglog-input-group">
+						<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+						<input id="username" class="form-control"  type="text" name="username" placeholder="Username"/>
+                                        </div>
+    				</div>
+                                <div class="form-group">
+                                    <label for="email">Email address</label>
+                                    <div class="input-group pb-modalreglog-input-group">
+                                        <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                                        <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <div class="input-group pb-modalreglog-input-group">
+                                        <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                                        <input type="password" class="form-control" id="pass" placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="confirmpassword">Confirm password</label>
+                                    <div class="input-group pb-modalreglog-input-group">
+                                        <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                                        <input type="password" class="form-control" id="confirmpass" placeholder="Confirm Password">
+                                    </div>
+                                </div>
+				<div class="form-group">
+					<label for = "phone">Contact Number</label><br />
+ 					<div class="input-group pb-modalreglog-input-group">
+                                        	<span class="input-group-addon"><span class="glyphicon glyphicon-phone"></span></span>
+						 <input type = "text" class="form-control" name = "phone" id="phoneno" maxlength = "10" placeholder = "Enter a valid phone number" pattern = "[0-9]{10}">
+       					</div>
+				</div>
+
+                                <div class="form-group">
+                                    <input type="checkbox" id="ch" name="chs">
+                                    I agree with <a href="#" style="color:orange">terms and conditions.</a>
+                                </div>
+				<div class="modal-footer">
+                            		<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            		<button type="submit" class="btn btn-warning">Sign up</button>
+                        	</div>
+                            </form>
+				<!-- form registration ended-->
+                        </div><!--modal-content -->
+
+                    </div><!-- "modal-dialog -->
+                </div><!-- modal fade -->
+            </div>
+
+	<!-- modals end -->
 	<!-- start footer -->
 	<footer>
 		<div class="container">
@@ -235,6 +363,34 @@
 	<script src="js/plugins.js"></script>
 	<script src="js/smoothscroll.js"></script>
 	<script src="js/custom.js"></script>
+	<script>
 
+	function Validation()
+	{
+	 var email = document.getElementById('inputEmail');
+	 var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+		if (!filter.test(email.value)) {
+			alert('Please provide a valid email address');
+			email.focus;
+		return false;
+	}
+	var pswd=document.getElementById('pass');
+	var cpswd=document.getElementById('confirmpass');
+	if(pswd!=cpswd){
+		alert("password does not match"); confirmpass.focus; return false;}
+
+			var phone = document.getElementById('phoneno');
+		 var phoneNum = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+					if(phone.value.match(phoneNum)) {
+							return true;
+					}
+					else {
+							alert('Please provide a valid phone number');
+				phoneno.focus;
+			return false;
+					}
+	}
+	</script>
 </body>
 </html>
