358176de6ce8d5aa029d0042f954045c7f9a1042
Vinay Sharma <vinay04sharma@icloud.com>
2017-10-07 10:32:35 +0530

    improved outlet accordion

--- admin.php
+++ admin.php
@@ -186,7 +186,7 @@ $outlet_array = array();
 						array_push($outlet_array, $document);
 						?>
 
-						<a class="list-group-item" data-toggle="collapse" data-target="<?php echo "#".$document['_id']; ?>" data-parent="#accordion">
+						<a class="list-group-item <?php if($index==0) {echo "disabled"; } ?>" data-toggle="collapse" data-target="<?php echo "#".$document['_id']; ?>" data-parent="#accordion">
 			    		<h4 class="list-group-item-heading accordion-toggle"><?php echo $document['outlet']?>
 								<button type="button" class="btn btn-danger btn-space pull-right" id="<?php echo $index; ?>" onclick="deleteOutlet(this);"><span class="glyphicon glyphicon-remove"></span> </button>
 								<button type="button" name="edit_modal" class="btn btn-default btn-space pull-right" data-toggle="modal" data-target="#outletEditModal" onclick="putContents(this); " id="<?php echo $index; ?>" ><span class="glyphicon glyphicon-pencil"></span> </button>
@@ -362,8 +362,9 @@ $outlet_array = array();
 <!-- Outlet Modal End -->
 
 	<script>
-
-
+			$(".list-group-item").on("click", function() {
+				$(this).toggleClass("disabled");
+			});
 
 			function deleteOutlet(param) {
 
