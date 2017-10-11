5f9f63c60458676694e0f99ddcea123ae104e86f
Vinay Sharma <vinay04sharma@icloud.com>
2017-10-11 10:37:50 +0530

    added search

--- data.php
+++ data.php
@@ -1,4 +1,40 @@
 <?php
-  
-  var_dump($_POST);
+
+
+  $q = $_POST["suggest"];
+  $search_by = $_POST["search_by"];
+  //;var_dump($_POST);
+
+  $m = new MongoClient("mongodb://admin:EIIGMGVVORZLANRD@sl-eu-lon-2-portal.5.dblayer.com:20539,sl-eu-lon-2-portal.0.dblayer.com:20539/admin?ssl=true");
+  $db = $m->Pizza;
+  $collection = $db->users;
+
+  $result = $collection->find(array($search_by => new MongoRegex("/$q/i")));
+?>
+
+
+<div class="panel list-group">
+
+<?php
+      if ($result->count() != 0 AND !empty($q)) {
+      foreach ($result as $row) { ?>
+
+    <a class="list-group-item" data-toggle="collapse" data-target="<?php echo "#".$row['_id']; ?>" data-parent="#accordion">
+      <h4 class="list-group-item-heading accordion-toggle">
+        <?php echo $row["username"]; ?>
+        <button type="button" class="btn btn-danger btn-space pull-right" id="<?php echo $index; ?>" onclick="deleteOutlet(this);"><span class="glyphicon glyphicon-remove"></span> </button>
+        <button type="button" name="edit_modal" class="btn btn-default btn-space pull-right" data-toggle="modal" data-target="#outletEditModal"   ><span class="glyphicon glyphicon-pencil"></span> </button>
+      </h4>
+        <?php echo '<p>'.$row['email'].'</p>'; ?>
+    </a>
+    <div id="<?php echo $row['_id']; ?>" class="sublinks collapse">
+      some data
+    </div>
+
+<?php
+  }
+} else {
+  echo '<p>No Suggestions</p>';
+}
  ?>
+ </div>
--- dynamic.php
+++ dynamic.php
@@ -1,24 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<?php include("header.php"); ?>
 <script>
 $(document).ready(function(){
-    $("input").keyup(function(){
+    $("input").on('keyup change', function(){
         txt = $("input").val();
-        $("span").load("data.php", {suggest: txt});
+        search_by = $("input:radio:checked").val();
+        $("#accordion").load("data.php", {suggest: txt, search_by});
     });
 });
 </script>
 </head>
 <body>
 
-<p>Start typing a name in the input field below:</p>
-First name:
+<div class="container pb-modalreglog-container">
 
-<input type="text">
 
-<p>Suggestions: <span></span></p>
+  <div class="panel panel-default">
+    <div class="panel-heading"><h3>Outlets</h3></div>
+    <input type="text">
+    <input type="radio" name="search" value="username" checked>Username<br>
+    <input type="radio" name="search" value="email">Email<br>
+    <input type="radio" name="search" value="address">Address
+
+      <div class="panel-body" id="accordion">
+
+
+      </div>
+
+    </div>
+  </div>
 
 
 </body>
